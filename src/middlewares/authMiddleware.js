b1db78ae1007df8b20f9f4bb6e600215fd84f8c0
María Delgado <mariad1996@gmail.com>
2024-04-30 18:48:50 +0200

    middleware complete

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,16 +1,23 @@
-// const { generateHtml } = require("../utils/helperFunctions")
-// const { unauthorizedUser, notLoggedIn } = require("../utils/htmlTemplates")
+const notLoggedIn = {
+    title: 'No login detected',
+    message: 'This content is only accessible to administrators. If you wish to access this content, log in with an admin account'
+}
 
-// function checkAuthState(req, res, next) {
-//     if (!res.headersSent) {
-//         if (req.originalUrl.includes('/shop/admin') && req.originalUrl !== '/shop/logout') {
-//             if (!req.session.uid || !req.session.role) {
-//                 res.status(200).json(generateHtml(notLoggedIn, req))
-//             } else if (req.session.uid && req.session.role === 'user') {
-//                 res.status(200).json(generateHtml(unauthorizedUser, req))
-//             } else { next() }
-//         } else { next() } 
-//     } else { next() } 
-// }
+const unauthorizedUser = {
+    title: 'Unauthorized user',
+    message: 'Your user does not have administrator privileges. If you wish to access this content, please log out of the current session and log in with an admin account.'
+}
 
-// module.exports = checkAuthState
+function checkAuthState(req, res, next) {
+    if (!res.headersSent) {
+        if (req.originalUrl.includes('/admin') && req.originalUrl !== '/logout') {
+            if (!req.session.uid || !req.session.role) {
+                res.status(200).json(notLoggedIn)
+            } else if (req.session.uid && req.session.role === 'user') {
+                res.status(200).json(unauthorizedUser)
+            } else { next() }
+        } else { next() }
+    } else { next() }
+}
+
+module.exports = checkAuthState
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const authRoutes = require('./authRoutes')
 const productRoutes = require('./productRoutes')
 const adminRoutes = require('./adminRoutes')
-// const checkAuthState = require('../middlewares/authMiddleware')
+const checkAuthState = require('../middlewares/authMiddleware')
 
 router.use('/', productRoutes)
 router.use('/', authRoutes)
-router.use('/', adminRoutes)
+// router.use('/', adminRoutes)
 
-// router.use('/', checkAuthState, adminRoutes)
+router.use('/', checkAuthState, adminRoutes)
 
 module.exports = router
